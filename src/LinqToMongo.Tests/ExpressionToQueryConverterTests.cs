9779993e4286f14d69cabb57a7c0b3222cb618f8
elemarjr@gmail.com <elemarjr@cxj0147.promob.local>
2012-02-24 11:17:16 -0200

    Added SharpTestsEx (nuget) reference

--- src/LinqToMongo.Tests/ExpressionToQueryConverterTests.cs
+++ src/LinqToMongo.Tests/ExpressionToQueryConverterTests.cs
@@ -2,10 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NUnit.Framework;
+using MongoDB.Driver.Builders;
+using SharpTestsEx;
 
 namespace LinqToMongo.Tests
 {
+    [TestFixture]
     public class ExpressionToQueryConverterTests
     {
+        [Test]
+        public void SimpleEQ()
+        {
+            var target = ExpressionToQueryConverter.Convert(d => d["name"] == "John");
+            var q = Query.EQ("name", "John");
+            target
+                .ToString()
+                .Should().Be(q.ToString());
+        }
     }
 }
--- src/LinqToMongo/ExpressionToQueryConverter.cs
+++ src/LinqToMongo/ExpressionToQueryConverter.cs
@@ -2,10 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MongoDB.Driver;
+using System.Linq.Expressions;
+using MongoDB.Bson;
+using MongoDB.Driver.Builders;
 
 namespace LinqToMongo
 {
-    public class ExpressionToQueryConverter
+    public static class ExpressionToQueryConverter
     {
+        public static QueryComplete Convert(
+            Expression<Func<BsonDocument, bool>> expression
+            )
+        {
+            return Query.EQ("name", "John");
+        }
+
     }
 }
