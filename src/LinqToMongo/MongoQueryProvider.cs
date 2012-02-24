8ba56db9d0e264888b066e6bd2054825a1be7858
elemarjr@gmail.com <elemarjr@cxj0147.promob.local>
2012-02-24 14:27:37 -0200

    Starting LINQ Query Provider

---
+++ src/LinqToMongo/MongoQueryProvider.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace LinqToMongo
+{
+    public class MongoQueryProvider : IQueryProvider
+    {
+        #region IQueryProvider Members
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable CreateQuery(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TResult Execute<TResult>(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object Execute(Expression expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ src/LinqToMongo/QueryableMongo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using MongoDB.Bson;
+
+namespace LinqToMongo
+{
+    public class QueryableMongo : IOrderedQueryable<BsonDocument>
+    {
+        #region IOrderedQueryable<BsonDocument> Members
+
+        public IEnumerator<BsonDocument> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Type ElementType
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public Expression Expression
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public IQueryProvider Provider
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        #endregion
+    }
+}
